49afa2548ef3abd22fb65d9ebdf2257466a51567
Amer Ansari <amer@savandbros.com>
2019-07-27 00:12:21 +0400

    Setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SpendComponent } from './components/shared/spend/spend.component';
+import { EarnComponent } from './components/shared/earn/earn.component';
+import { TotalComponent } from './components/shared/total/total.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SpendComponent,
+    EarnComponent,
+    TotalComponent,
   ],
   imports: [
     BrowserModule,
