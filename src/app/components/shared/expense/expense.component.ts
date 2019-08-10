61e8cad174a3e76f262f198a253fd86549aeec22
Amer Ansari <amer@savandbros.com>
2019-08-10 15:30:14 +0400

    Change view

--- src/app/components/shared/expense/expense.component.ts
+++ src/app/components/shared/expense/expense.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Expense } from 'src/app/models/expense';
 
 @Component({
-  selector: 'app-spend',
+  selector: 'app-expense',
   templateUrl: './expense.component.html',
   styleUrls: ['./expense.component.scss']
 })
