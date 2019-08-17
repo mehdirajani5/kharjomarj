042e226495aba5f547dc577785a663d40fb8c2dc
Amer Ansari <amer@savandbros.com>
2019-08-17 04:53:09 +0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
+import { CollapseModule } from 'ngx-bootstrap';
+import { AddComponent } from 'src/app/components/shared/add/add.component';
+import { ExpenseComponent } from 'src/app/components/shared/expense/expense.component';
+import { IncomeComponent } from 'src/app/components/shared/income/income.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { IncomeComponent } from 'src/app/components/shared/income/income.component';
-import { ExpenseComponent } from 'src/app/components/shared/expense/expense.component';
-import { AddComponent } from 'src/app/components/shared/add/add.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,8 @@ import { AddComponent } from 'src/app/components/shared/add/add.component';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
     ReactiveFormsModule,
   ],
   providers: [],
@@ -32,6 +37,7 @@ import { AddComponent } from 'src/app/components/shared/add/add.component';
 export class AppModule {
   static readonly icons = [
     faPlus,
+    faTrash,
   ];
 
   constructor() {
